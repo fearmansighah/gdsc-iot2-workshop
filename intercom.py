6036947981924e4319820ec8cfaf2edaecbfc73a
nazarovenko <efirmansyah.ca@gmail.com>
2021-12-28 17:51:46 -0800

    commit on 'main'

---
+++ intercom.py
@@ -0,0 +1,72 @@
+import paho.mqtt.client as mqtt
+from personal_broker import broker
+from time import sleep
+
+
+def intercom_pub(topic, msg, broker, port, client):
+    client.connect(broker, port)
+    print('connected to broker')
+    client.publish(topic, msg)
+    print(f'just published: {msg}')
+
+
+def on_message(client, userdata, msg):
+    request = str(msg.payload.decode("utf-8"))
+
+    if request == 'y':
+        print('door unlocked!')
+        sleep(10)
+        print('door locked')
+        client.disconnect()
+    elif request == 'n':
+        print('door access not granted :(')
+        client.disconnect()
+    else:
+        pass
+
+
+def intercom_listen(topic, client):
+    client.loop_start()
+    client.subscribe(topic)
+    print(f'subscribed to: {topic}')
+
+
+    client.on_message = on_message
+    sleep(60)
+
+    client.unsubscribe(topic)
+    client.disconnect()
+    print('disconnecting from broker...')
+
+
+def main():
+
+    #mqttBroker = "mqtt.eclipseprojects.io"
+    mqttBroker = broker
+
+    roomNumber = str(input('input room number: '))
+    print('\n')
+
+    cmdPrompt = 'input: \n1 - to request door access \n2 - to request chat \n'
+    cmd = str(input(cmdPrompt))
+    print('\n')
+
+    intercom=mqtt.Client('intercom')
+    intercom.username_pw_set(username="edwin", password="ids_2021")
+
+    intercom_pub(
+                topic=f'gdsc_iot2/{roomNumber}',
+                msg=cmd,
+                broker=mqttBroker,
+                port=1883,
+                client=intercom
+                )
+
+    intercom_listen(
+                topic=f'gdsc_iot2/{roomNumber}',
+                client=intercom
+    )
+    
+    
+if __name__ == "__main__":
+    main()
---
+++ room2.py
@@ -0,0 +1,65 @@
+import paho.mqtt.client as mqtt
+from personal_broker import broker
+from time import sleep
+
+
+def room_pub(topic, msg, client):
+    client.publish(topic, msg)
+    print(f'just published: {msg}')
+
+
+def on_message(client, userdata, msg):
+    request = str(msg.payload.decode("utf-8"))
+
+    if request == '1':
+        grant = str(input('intercom asking for door access, grant? (y/n): '))
+
+        roomNumber = '2'
+        room_pub(
+                topic=f'gdsc_iot2/{roomNumber}',
+                msg=grant,
+                client=client
+                )
+
+    else:
+        pass
+
+
+def room_listen(client, topic, broker, port):
+    
+    client.connect(broker, port)
+    print('connected to broker')
+    
+    client.loop_start()
+
+    client.subscribe(topic)
+    print(f'subscribed to: {topic}')
+
+    client.on_message = on_message
+
+    sleep(30)
+
+    client.unsubscribe(topic)
+    client.disconnect()
+    print('disconnecting from broker...')
+
+
+def main():
+    #mqttBroker = "mqtt.eclipseprojects.io"
+    mqttBroker = broker
+    
+    
+    roomNumber = '2'
+    room2=mqtt.Client(f'room {roomNumber}')
+    room2.username_pw_set(username="edwin", password="ids_2021")
+
+    room_listen(
+                topic=f'gdsc_iot2/{roomNumber}',
+                broker=mqttBroker,
+                port=1883,
+                client=room2
+                )
+    
+
+if __name__ == "__main__":
+    main()
