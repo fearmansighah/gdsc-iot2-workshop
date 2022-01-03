184c884f23c8790c017bd1aef8083a66cce00e60
nazarovenko <efirmansyah.ca@gmail.com>
2022-01-03 01:09:27 -0800

    added loop for entire program

--- intercom.py
+++ intercom.py
@@ -12,62 +12,66 @@ def intercom_pub(topic, msg, broker, port, client):
 
 def on_message(client, userdata, msg):
     request = str(msg.payload.decode("utf-8"))
-    
 
     if request == 'y':
         print('door unlocked!')
-        sleep(10)
+        sleep(5)
         print('door locked')
-        client.disconnect()
     elif request == 'n':
         print('door access not granted :(')
-        client.disconnect()
     else:
         pass
 
+    print('\n')
+ 
 
 def intercom_listen(topic, client):
+
     client.loop_start()
     client.subscribe(topic)
     print(f'subscribed to: {topic}')
 
-
     client.on_message = on_message
-    sleep(60)
+    sleep(10) # whenever a callback is received, wait 10 seconds before stopping the loop
 
-    client.unsubscribe(topic)
-    client.disconnect()
-    print('disconnecting from broker...')
+    client.loop_stop()
+
 
 
 def main():
 
     #mqttBroker = "mqtt.eclipseprojects.io"
     mqttBroker = broker
-
-    roomNumber = str(input('input room number: '))
-    print('\n')
-
-    cmdPrompt = 'input: \n1 - to request door access \n2 - to request chat \n'
-    cmd = str(input(cmdPrompt))
-    print('\n')
-
     intercom=mqtt.Client('intercom')
     intercom.username_pw_set(username="edwin", password="ids_2021")
 
-    intercom_pub(
-                topic=f'gdsc_iot2/{roomNumber}',
-                msg=cmd,
-                broker=mqttBroker,
-                port=1883,
-                client=intercom
-                )
+    cmd = '1'
+    while cmd == '1':
 
-    intercom_listen(
-                topic=f'gdsc_iot2/{roomNumber}',
-                client=intercom
-    )
-    
+        roomNumber = str(input('input room number: '))
+        print('\n')
+
+        cmdPrompt = 'input: \n1 - to request door access \n2 - to close connection \n'
+        cmd = str(input(cmdPrompt))
+        print('\n')
+
+        if cmd == '1':
+            intercom_pub(
+                        topic=f'gdsc_iot2/{roomNumber}',
+                        msg=cmd,
+                        broker=mqttBroker,
+                        port=1883,
+                        client=intercom
+                        )
+
+            intercom_listen(
+                            topic=f'gdsc_iot2/{roomNumber}',
+                            client=intercom
+                            )
+        
+        else:
+            pass
+
     
 if __name__ == "__main__":
     main()
